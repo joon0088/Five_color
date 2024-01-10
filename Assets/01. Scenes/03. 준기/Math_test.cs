31475b0924558c65efd018caeda9c249ebb6ec50
DESKTOP-J51L3T6\hippo <leejooki0088@gmail.com>
2024-01-11 08:39:22 +0900

    2024 01 11 - 송편 점수 (2)
    2024 01 11 - 송편 점수 (2)

--- Assets/01. Scenes/03. 준기/Math_test.cs
+++ Assets/01. Scenes/03. 준기/Math_test.cs
@@ -1,41 +1,41 @@
-public void Result()
-{
-    if (Food[0] == 1)
-    {
-        score += 30;
-    }
-    else if (Food[0] == 2)
-    {
-        score += 50;
-    }
-    else if (Food[0] == 3)
-    {
-        score += 100;
-    }
+// public void Result()
+// {
+//     if (Food[0] == 1)
+//     {
+//         score += 30;
+//     }
+//     else if (Food[0] == 2)
+//     {
+//         score += 50;
+//     }
+//     else if (Food[0] == 3)
+//     {
+//         score += 100;
+//     }
 
-    if (Food[1] == 1)
-    {
-        score += 30;
-    }
-    else if (Food[1] == 2)
-    {
-        score += 50;
-    }
-    else if (Food[1] == 3)
-    {
-        score += 100;
-    }
+//     if (Food[1] == 1)
+//     {
+//         score += 30;
+//     }
+//     else if (Food[1] == 2)
+//     {
+//         score += 50;
+//     }
+//     else if (Food[1] == 3)
+//     {
+//         score += 100;
+//     }
 
-    if (Food[2] == 1)
-    {
-        score += 10;
-    }
-    else if (Food[2] == 2)
-    {
-        score += 20;
-    }
-    else if (Food[2] == 3)
-    {
-        score += 30;
-    }
-}
+//     if (Food[2] == 1)
+//     {
+//         score += 10;
+//     }
+//     else if (Food[2] == 2)
+//     {
+//         score += 20;
+//     }
+//     else if (Food[2] == 3)
+//     {
+//         score += 30;
+//     }
+// }
